33869168eaa0ad9bb517bd59bb9fff09d7ea58b1
geonwoo <gudrjsdn8825@naver.com>
2025-06-03 14:25:54 +0900

    Feat: 콘솔이 ANSI 이스케이프 시퀀스를 해석하도록 하는 메소드 추가 visual Studio 에서 디버그 없이 실행했을 때 뿐만 아니라, 실제 배포(release) 파일(.EXE) 에서도 화면을 지우는 ANSI 이스케이프 코드를 동작시키도록 함

--- Koflearn/Koflearn_main.cpp
+++ Koflearn/Koflearn_main.cpp
@@ -1,9 +1,33 @@
 ﻿#include <iostream>
+#include <windows.h>
+#include <cstdio>
 #include "koflearnPlatManager.h"
 using namespace std;
 
+// 화면을 지우는(Clear Screen) ANSI 이스케이프 코드 를 
+// 배포 파일(release EXE 파일) 에도 동작시키기 위한 함수 생성
+void enableAnsiColors() {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) {
+        return;
+    }
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) {
+        return;
+    }
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode)) {
+        return;
+    }
+}
+
 int main()
 {
+    // 콘솔이 ANSI 이스케이프 시퀀스를 해석하도록 함
+    enableAnsiColors();
+
     // koflearnPlatManager 구현체
     KoflearnPlatManager mainProgram;
 
